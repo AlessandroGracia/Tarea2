64ddebf482f47571a296c58ec588ed0376c785db
ALESSANDRO EMANUELLE GRACIA ALBAN <alessandro.gracia@pucese.edu.ec>
2020-11-11 19:36:43 -0500

    Revert "Funcion Suma de dos numero"
    This reverts commit 305d52693e9927a736b8448f6085f94df5533237.

--- Tarea2/Program.cs
+++ Tarea2/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,19 +11,7 @@ namespace Tarea2
         static void Main(string[] args)
         {
             Console.WriteLine("Hola planeta");
-            //HOLA ESMERALDAS
-            //feliz cumpleaños
-            //Alessandro Gracia
-            Console.WriteLine("Hola Puce");
-            Console.WriteLine("Probando la funcion");
-            Console.WriteLine(FunsionSuma(10, 20));
-        }
-        // Funcion Suma
-        int FuncionSuma(int a, int b);
-         {
-        return (a * 10 + b * 100);
-    }
-}
-    
 
+        }
+    }
 }
